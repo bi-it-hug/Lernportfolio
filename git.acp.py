17f2732f7af0121c09722088d23fa94277ed845c
Lorenzo Hug <lorenzo.hug@espas.ch>
2023-10-26 14:52:50 +0200

    update

--- git.acp.py
+++ git.acp.py
@@ -1,4 +1,4 @@
 import subprocess
 
-for command in ['git add *', f'git commit -m {"Commit message: "}', 'git push']:
+for command in ['git add *', f'git commit -m {input("Commit message: ")}', 'git push']:
     subprocess.run(command, shell=True)
